7be36a6d27c4c56dfe0bd93c3057f5523d04bebb
root <root@EAGLERIDER.(none)>
2015-09-26 10:12:59 +0530

    Initial 3 tasks

--- app/helpers/accounts_helper.rb
+++ app/helpers/accounts_helper.rb
@@ -1,11 +1,10 @@
 module AccountsHelper
   def products_chart_data
-    orders_by_day = Account.total_grouped_by_day(30.days.ago)
+    orders_by_day = Account.total_grouped_by_day(30.days.ago,current_user)
+    rtn_ary = [{created_at: 31.days.ago.to_date, balance: 1000}]
     (30.days.ago.to_date..Date.today).map do |date|
-      {created_at: date, 
-        balance: orders_by_day[date].try(:first).try(:total_balance) || 0
-      }
+      rtn_ary << { created_at: date, balance: orders_by_day[date].try(:first).try(:total_balance) || rtn_ary.last[:balance]}
     end
+    rtn_ary
   end
-
 end
