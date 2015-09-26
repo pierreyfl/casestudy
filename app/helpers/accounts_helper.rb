7570afc5274feeb5e406c6b6ee7dceffe2e44c2a
root <root@EAGLERIDER.(none)>
2015-09-26 11:05:49 +0530

    changed start as first entry date

--- app/helpers/accounts_helper.rb
+++ app/helpers/accounts_helper.rb
@@ -1,8 +1,10 @@
 module AccountsHelper
   def products_chart_data
-    orders_by_day = Account.total_grouped_by_day(30.days.ago,current_user)
-    rtn_ary = [{created_at: 31.days.ago.to_date, balance: 1000}]
-    (30.days.ago.to_date..Date.today).map do |date|
+    first_date = Account.where(user_id: current_user.id).first
+    first_date = first_date.created_at.to_date
+    orders_by_day = Account.total_grouped_by_day(first_date,current_user)
+    rtn_ary = [{created_at: first_date.prev_day.to_date, balance: 1000}]
+    (first_date..Date.today).map do |date|
       rtn_ary << { created_at: date, balance: orders_by_day[date].try(:first).try(:total_balance) || rtn_ary.last[:balance]}
     end
     rtn_ary
