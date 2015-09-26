579524bdc1d38490eb70993ccf272eb4ccf22bb3
root <root@EAGLERIDER.(none)>
2015-09-26 11:41:35 +0530

    30 days logic

--- app/helpers/accounts_helper.rb
+++ app/helpers/accounts_helper.rb
@@ -1,10 +1,14 @@
 module AccountsHelper
   def products_chart_data
-    first_date = Account.where(user_id: current_user.id).first
-    first_date = first_date.created_at.to_date
-    orders_by_day = Account.total_grouped_by_day(first_date,current_user)
-    rtn_ary = [{created_at: first_date.prev_day.to_date, balance: 1000}]
-    (first_date..Date.today).map do |date|
+    orders_by_day = Account.total_grouped_by_day(30.days.ago,current_user)
+    first_entry = Account.where("user_id='"+current_user.id.to_s+"' and created_at<='"+31.days.ago.strftime("%Y-%m-%d 23:59:59")+"'").last
+    if first_entry
+     rtn_ary = [{created_at: 31.days.ago.to_date, balance: first_entry.balance}]
+    else
+     rtn_ary = [{created_at: 31.days.ago.to_date, balance: 0}]
+    end
+    orders_by_day = Account.total_grouped_by_day(31.days.ago.to_date,current_user)
+    (30.days.ago.to_date..Date.today).map do |date|
       rtn_ary << { created_at: date, balance: orders_by_day[date].try(:first).try(:total_balance) || rtn_ary.last[:balance]}
     end
     rtn_ary
